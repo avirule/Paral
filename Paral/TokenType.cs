f505843b6eef231334109d6854543e55227e5083
Zavier Divelbiss <semiviral@gmail.com>
2020-05-30 10:13:26 -0500

    abstraction

---
+++ Paral/TokenType.cs
@@ -0,0 +1,16 @@
+namespace Paral
+{
+    public enum TokenType
+    {
+        Identifier,
+        Operator,
+        CharacterLiteral,
+        StringLiteral,
+        NumericLiteral,
+        DecimalLiteral,
+        ControlFlow,
+        SingularFlow,
+        NewLine,
+        EndOfFile
+    }
+}
